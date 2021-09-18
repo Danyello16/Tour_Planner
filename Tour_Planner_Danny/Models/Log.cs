01da9515d724b6d18d1eb3fc48dc0c2569643700
Danyello16 <danyellosan@gmail.com>
2021-09-18 21:13:56 +0300

    Models Finished..

---
+++ Tour_Planner_Danny/Models/Log.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tour_Planner_Danny.Models
+{
+    public class Log
+    {
+        public int LogID { get; set; }
+        public int TourID { get; set; }  
+        
+        public int Rating { get; set; }
+        public string TotalTime { get; set; }
+        public string Date { get; set; }
+        public string Distance { get; set; }
+
+        public string Temperature { get; set; }
+        public string Cadence { get; set; }
+        public string PositionLong { get; set; }
+        public string PositionLat { get; set; }
+        public string AirPower { get; set; }
+    }
+}
---
+++ Tour_Planner_Danny/Models/Tour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace Tour_Planner_Danny.Models
+{
+    public class Tour
+    {
+        public int TourID { get; set; }
+        public string TourName { get; set; }
+        public string TourDescription { get; set; }
+        public string Route { get; set; }
+        public string Distance { get; set; }
+        public ObservableCollection<Log> Logs { get; set; }
+        public Tour()
+        {
+            Logs = new ObservableCollection<Log>();
+        }
+    }
+}
